bacbe9fc8fba28324d96013dfb90cd7562dcc28a
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-11-23 21:34:12 +0300

    Добавил использование MediatR

--- Dotnet.Homeworks.MainProject/Program.cs
+++ Dotnet.Homeworks.MainProject/Program.cs
@@ -1,7 +1,10 @@
 using Dotnet.Homeworks.Data.DatabaseContext;
 using Dotnet.Homeworks.MainProject.Configuration;
 using Dotnet.Homeworks.MainProject.Services;
+using Dotnet.Homeworks.MainProject.ServicesExtensions.DataAccess;
 using Dotnet.Homeworks.MainProject.ServicesExtensions.Masstransit;
+using Dotnet.Homeworks.MainProject.ServicesExtensions.MediatR;
+using Dotnet.Homeworks.MainProject.ServicesExtensions.Migrations;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,12 +12,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddDataAccessLayer();
 
 builder.Services.AddMasstransitRabbitMq(
     builder.Configuration.GetSection("RabbitMqConfig").Get<RabbitMqConfig>()!
 );
+
+builder.Services.AddMediatR();
 
 builder.Services.AddSingleton<IRegistrationService, RegistrationService>();
 builder.Services.AddSingleton<ICommunicationService, CommunicationService>();
@@ -31,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+await app.MigrateIfNeededAsync();
 
 app.MapGet("/", () => "Hello World!");
 
